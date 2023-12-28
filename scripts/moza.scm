a30b300de121ebe57ffa3f539145678188a56ee7
natukin1978 <natukin1978@hotmail.com>
2023-12-29 02:51:12 +0900

    Overwrite and save the image at the end.
    However, the process of closing the image fails.

--- scripts/moza.scm
+++ scripts/moza.scm
@@ -13,19 +13,26 @@
     (gimp-hue-saturation drawable hue-range hue-offset lightness saturation)
     (gimp-displays-flush)))
 
+(define (save-and-close-image image drawable)
+  (let* ((filename (car (gimp-image-get-filename image))))
+    (gimp-file-save RUN-NONINTERACTIVE image drawable filename filename)
+    (gimp-image-clean-all image)
+    (gimp-image-delete image)))
+
 (define (apply-effects image drawable)
   (gimp-image-undo-group-start image)
   (mosaic-script image drawable)
   (hue-chroma-script image drawable)
   (gimp-image-undo-group-end image)
-  (gimp-displays-flush))
+  (gimp-displays-flush)
+  (save-and-close-image image drawable))
 
 (script-fu-register "apply-effects"
-                   "<Image>/Filters/MyScripts/Moza"
-                   "Apply mosaic and adjust hue-saturation"
-                   "Your Name"
-                   "Your Name"
-                   "2023"
-                   "*"
-                   SF-IMAGE "Image" 0
-                   SF-DRAWABLE "Drawable" 0)
+                    "<Image>/Filters/MyScripts/Moza"
+                    "Apply mosaic and adjust hue-saturation, then save and close"
+                    "Your Name"
+                    "Your Name"
+                    "2023"
+                    "*"
+                    SF-IMAGE "Image" 0
+                    SF-DRAWABLE "Drawable" 0)
